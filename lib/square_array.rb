f60e7dbecb17c9b053db2c71817c3eb0424c131d
isabel k. lee <hello@isabelklee.co>
2020-01-27 05:17:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length do 
-    puts array_sq.push(array[counter] * array[counter])
+    array_sq.push(array[counter] * array[counter])
     counter += 1 
   end 
 end
